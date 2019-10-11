b92f9c251ac181adcf1867488b81705936976e04
Rémon <remonhelmond@gmail.com>
2019-10-11 19:48:09 +0200

    create project structure

---
+++ test/app_center_uploader_test.dart
@@ -0,0 +1,16 @@
+import 'package:app_center_uploader/src/app_center_uploader_base.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
