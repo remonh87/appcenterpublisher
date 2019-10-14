f0d77fe537100785af8fc04316de13b4e25ca399
Rémon <remonhelmond@gmail.com>
2019-10-14 20:11:26 +0200

    handle response upload release api call

--- lib/src/create_upload_url.dart
+++ lib/src/create_upload_url.dart
@@ -1,22 +1,27 @@
 import 'dart:convert';
 
+import 'package:app_center_uploader/src/api_responses.dart';
 import 'package:app_center_uploader/src/model/api_config.dart';
 import 'package:app_center_uploader/src/model/release_info.dart';
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 
-void createUploadUrl({
+Future<ReleaseUploadResponse> createUploadUrl({
   @required Future<http.Response> Function(dynamic url, {Map<String, String> headers, dynamic body}) callApi,
   @required ApiConfig config,
   @required ReleaseInfo releaseInfo,
 }) {
   assert(callApi != null);
 
-  callApi(
+  const decoder = JsonDecoder();
+  return callApi(
     'https://api.appcenter.ms/v0.1/apps/${config.owner}/${releaseInfo.appName}/release_uploads',
     headers: {'Content-Type': 'application/json', 'X-API-Token': '${config.apiToken}'},
     body: _createJsonBody(releaseInfo),
-  );
+  ).then((response) {
+    final bodyconverted = decoder.convert(response.body) as Map<String, dynamic>;
+    return ReleaseUploadResponse(bodyconverted['upload_id'].toString(), bodyconverted['upload_url'].toString());
+  });
 }
 
 String _createJsonBody(ReleaseInfo info) {
