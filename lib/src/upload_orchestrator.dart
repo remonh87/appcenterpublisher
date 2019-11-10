fc927dcbfacd047ed5d3bfca2af265504a1e8148
Rémon <remonhelmond@gmail.com>
2019-11-10 15:05:11 +0100

    add commit upload to orchestrator

--- lib/src/upload_orchestrator.dart
+++ lib/src/upload_orchestrator.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_center_uploader/src/api_operation_data.dart';
 import 'package:app_center_uploader/src/model/api_config.dart';
 import 'package:app_center_uploader/src/model/release_info.dart';
@@ -5,7 +7,7 @@ import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
 class UploadOrchestrator {
-  const UploadOrchestrator({@required this.createUploadUrl, @required this.uploadBinary});
+  const UploadOrchestrator({@required this.createUploadUrl, @required this.uploadBinary, @required this.commitUpload});
 
   final Future<ReleaseUploadResult> Function({
     @required Future<http.Response> Function(dynamic url, {Map<String, String> headers, dynamic body}) callApi,
@@ -13,17 +15,39 @@ class UploadOrchestrator {
     @required AppRelease appRelease,
   }) createUploadUrl;
 
+  final Future<CommitReleaseResult> Function(
+      {@required
+          Future<http.Response> Function(dynamic url, {Map<String, String> headers, dynamic body, Encoding encoding})
+              patch,
+      @required
+          String uploadId,
+      @required
+          ApiConfig config,
+      @required
+          String appName}) commitUpload;
+
   final Future<UploadBinaryResult> Function(String uploadUrl, String filePath) uploadBinary;
 
   Future<int> run(ApiConfig config, AppRelease appRelease) async {
     final result = await createUploadUrl(callApi: http.post, config: config, appRelease: appRelease);
 
-    //TODO parse file and remove stub
-    return await result.iswitch(success: (s) => _uploadBinary(s.uploadUrl), failure: (_) => 1);
+    return await result.iswitch(
+        success: (createReleaseResult) async {
+          final uploadResult = await _uploadBinary(createReleaseResult.uploadUrl);
+          return uploadResult.iswitch(
+              success: (_) => _commitRelease(createReleaseResult.uploadId, config, appRelease.appName),
+              failure: (_) => 1);
+        },
+        failure: (_) => 1);
   }
 
-  Future<int> _uploadBinary(String uploadUrl) async {
-    final result = await uploadBinary(uploadUrl, 'stub');
+  Future<UploadBinaryResult> _uploadBinary(String uploadUrl) async {
+    return await uploadBinary(uploadUrl, 'stub');
+  }
+
+  Future<int> _commitRelease(String uploadId, ApiConfig config, String appName) async {
+    final result = await commitUpload(patch: http.patch, uploadId: uploadId, config: config, appName: appName);
+
     return result.iswitch(success: (_) => 0, failure: (_) => 1);
   }
 }
--- test/upload_orchestrator_test.dart
+++ test/upload_orchestrator_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_center_uploader/src/api_operation_data.dart';
 import 'package:app_center_uploader/src/model/api_config.dart';
 import 'package:app_center_uploader/src/model/release_info.dart';
@@ -15,6 +17,13 @@ abstract class _UploadStub {
   });
 
   Future<UploadBinaryResult> uploadBinary(String uploadUrl, String filePath);
+
+  Future<CommitReleaseResult> commitUpload(
+      {Future<http.Response> Function(dynamic url, {Map<String, String> headers, dynamic body, Encoding encoding})
+          patch,
+      String uploadId,
+      ApiConfig config,
+      String appName});
 }
 
 class _MockUploader extends Mock implements _UploadStub {}
@@ -28,14 +37,30 @@ void main() {
 
   setUp(() {
     uploader = _MockUploader();
-    sut = UploadOrchestrator(createUploadUrl: uploader.createUploadUrl, uploadBinary: uploader.uploadBinary);
+    sut = UploadOrchestrator(
+        createUploadUrl: uploader.createUploadUrl,
+        uploadBinary: uploader.uploadBinary,
+        commitUpload: uploader.commitUpload);
   });
 
   group('Start upload', () {
     setUp(() async {
       when(uploader.createUploadUrl(
-              callApi: anyNamed('callApi'), config: anyNamed('config'), appRelease: anyNamed('appRelease')))
-          .thenAnswer((_) => Future.value(const ReleaseUploadResult.failure(ApiOperationFailure(message: ''))));
+        callApi: anyNamed('callApi'),
+        config: anyNamed('config'),
+        appRelease: anyNamed('appRelease'),
+      )).thenAnswer((_) => Future.value(const ReleaseUploadResult.failure(ApiOperationFailure(message: ''))));
+
+      when(uploader.uploadBinary(any, any))
+          .thenAnswer((_) => Future.value(const UploadBinaryResult.failure(ApiOperationFailure(message: ''))));
+
+      when(uploader.commitUpload(
+        patch: anyNamed('patch'),
+        uploadId: anyNamed('uploadId'),
+        appName: anyNamed('appName'),
+        config: anyNamed('config'),
+      )).thenAnswer((_) => Future.value(const CommitReleaseResult.failure(ApiOperationFailure(message: ''))));
+      ;
 
       await sut.run(config, release);
     });
@@ -45,11 +70,17 @@ void main() {
     });
 
     group('GIVEN create uploadurl succeeds', () {
+      final uploadId = 206;
       setUp(() async {
         when(uploader.createUploadUrl(
                 callApi: anyNamed('callApi'), config: anyNamed('config'), appRelease: anyNamed('appRelease')))
             .thenAnswer((_) => Future.value(
-                const ReleaseUploadResult.success(ReleaseUploadOperationSuccess('1', 'http://upload.test'))));
+                ReleaseUploadResult.success(ReleaseUploadOperationSuccess(uploadId.toString(), 'http://upload.test'))));
+      });
+
+      test('It executes uploading binary with correct arguments', () async {
+        await sut.run(config, release);
+        verify(uploader.uploadBinary('http://upload.test', 'stub')).called(1);
       });
 
       group('AND uploadBinary succeeds', () {
@@ -60,14 +91,68 @@ void main() {
           await sut.run(config, release);
         });
 
-        test('It executes uploading binary with correct arguments', () async {
-          verify(uploader.uploadBinary('http://upload.test', 'stub')).called(1);
+        test('It executes commit upload with correct arguments', () async {
+          verify(uploader.commitUpload(
+                  patch: http.patch, uploadId: uploadId.toString(), config: config, appName: 'test'))
+              .called(1);
         });
 
-        test('It returns exit code 0', () async {
-          final result = await sut.run(config, release);
+        group('AND commit release succeeds', () {
+          setUp(() {
+            when(
+              uploader.commitUpload(
+                  patch: anyNamed('patch'),
+                  uploadId: anyNamed('uploadId'),
+                  config: anyNamed('config'),
+                  appName: anyNamed('appName')),
+            ).thenAnswer((_) => Future.value(CommitReleaseResult.success(CommitReleaseSuccess())));
+          });
 
-          expect(result, 0);
+          test('It returns exit code 0', () async {
+            final result = await sut.run(config, release);
+            expect(result, 0);
+          });
+        });
+
+        group('AND commit release fails', () {
+          setUp(() {
+            when(
+              uploader.commitUpload(
+                  patch: anyNamed('patch'),
+                  uploadId: anyNamed('uploadId'),
+                  config: anyNamed('config'),
+                  appName: anyNamed('appName')),
+            ).thenAnswer(
+                (_) => Future.value(CommitReleaseResult.failure(ApiOperationFailure(message: 'commit failed'))));
+          });
+
+          test('It returns exit code 1®', () async {
+            final result = await sut.run(config, release);
+            expect(result, 1);
+          });
+        });
+      });
+
+      group('AND uploadBinary fails', () {
+        int result = -100;
+
+        setUp(() async {
+          when(uploader.uploadBinary(any, any))
+              .thenAnswer((_) => Future.value(UploadBinaryResult.failure(ApiOperationFailure(message: 'failed'))));
+          result = await sut.run(config, release);
+        });
+
+        test('It returns exitcode 1', () async {
+          expect(result, 1);
+        });
+
+        test('It does not call commit release', () {
+          verifyNever(uploader.commitUpload(
+            patch: anyNamed('patch'),
+            appName: anyNamed('appName'),
+            uploadId: anyNamed('uploadId'),
+            config: anyNamed('config'),
+          ));
         });
       });
     });
