312c4409c03948d71c7df2d38a91bdc6459ab8cb
Rémon <remonhelmond@gmail.com>
2019-11-14 21:01:32 +0100

    add distribute to group to test orchestrator

--- lib/src/upload_orchestrator.dart
+++ lib/src/upload_orchestrator.dart
@@ -2,12 +2,18 @@ import 'dart:convert';
 
 import 'package:app_center_uploader/src/api_operation_data.dart';
 import 'package:app_center_uploader/src/model/api_config.dart';
+import 'package:app_center_uploader/src/model/distribution_group.dart';
 import 'package:app_center_uploader/src/model/release_info.dart';
 import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
 class UploadOrchestrator {
-  const UploadOrchestrator({@required this.createUploadUrl, @required this.uploadBinary, @required this.commitUpload});
+  const UploadOrchestrator({
+    @required this.createUploadUrl,
+    @required this.uploadBinary,
+    @required this.commitUpload,
+    @required this.distributeToGroup,
+  });
 
   final Future<ReleaseUploadResult> Function({
     @required Future<http.Response> Function(dynamic url, {Map<String, String> headers, dynamic body}) callApi,
@@ -28,6 +34,14 @@ class UploadOrchestrator {
 
   final Future<UploadBinaryResult> Function(String uploadUrl, String filePath) uploadBinary;
 
+  final Future<DistributionResult> Function({
+    @required Future<http.Response> Function(dynamic url, {Map<String, String> headers, dynamic body}) post,
+    @required DistributionGroup distributionGroup,
+    @required ApiConfig config,
+    @required String appName,
+    @required int releaseId,
+  }) distributeToGroup;
+
   Future<int> run(ApiConfig config, AppRelease appRelease) async {
     final result = await createUploadUrl(callApi: http.post, config: config, appRelease: appRelease);
 
@@ -35,18 +49,35 @@ class UploadOrchestrator {
         success: (createReleaseResult) async {
           final uploadResult = await _uploadBinary(createReleaseResult.uploadUrl);
           return uploadResult.iswitch(
-              success: (_) => _commitRelease(createReleaseResult.uploadId, config, appRelease.appName),
+              success: (_) async {
+                final commitResult = await _commitRelease(createReleaseResult.uploadId, config, appRelease.appName);
+                return commitResult.iswitch(
+                    success: (c) => _distributeToGroup(config, c.releaseId, appRelease.appName), failure: (_) => 1);
+              },
               failure: (_) => 1);
         },
         failure: (_) => 1);
   }
 
   Future<UploadBinaryResult> _uploadBinary(String uploadUrl) async {
+    //TODO remove stub
     return await uploadBinary(uploadUrl, 'stub');
   }
 
-  Future<int> _commitRelease(String uploadId, ApiConfig config, String appName) async {
-    final result = await commitUpload(patch: http.patch, uploadId: uploadId, config: config, appName: appName);
+  Future<CommitReleaseResult> _commitRelease(String uploadId, ApiConfig config, String appName) async {
+    return await commitUpload(patch: http.patch, uploadId: uploadId, config: config, appName: appName);
+  }
+
+  Future<int> _distributeToGroup(ApiConfig config, int releaseId, String appName) async {
+    //TODO remove stub
+    final distributiongroup = DistributionGroup(id: 'stub');
+    final result = await distributeToGroup(
+      post: http.post,
+      distributionGroup: distributiongroup,
+      config: config,
+      appName: appName,
+      releaseId: releaseId,
+    );
 
     return result.iswitch(success: (_) => 0, failure: (_) => 1);
   }
