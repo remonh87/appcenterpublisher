32eafeb0438a357a9b91a1c85094ddfa86c48b23
Rémon <remonhelmond@gmail.com>
2019-11-17 14:51:26 +0100

    create yaml parser for parsing the congfig

---
+++ test/parse_yaml_test.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:app_center_uploader/src/parse_yaml.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+abstract class _Logger {
+  void print(Object object);
+}
+
+class _MockLogger extends Mock implements _Logger {}
+
+class _Mockfile extends Mock implements File {}
+
+void main() {
+  _Mockfile yamlFile;
+  _MockLogger logger;
+
+  group('$parseYamlConfig', () {
+    setUp(() {
+      yamlFile = _Mockfile();
+      logger = _MockLogger();
+    });
+
+    group('GIVEN yaml is valid', () {
+      setUp(() {
+        when(yamlFile.readAsStringSync()).thenReturn('appName: testApp\ndistributionGroup: aaa\nowner: test');
+      });
+
+      test('It returns the mapp with 3 items', () {
+        expect(parseYamlConfig(logger.print, yamlFile), {
+          'appName': 'testApp',
+          'distributionGroup': 'aaa',
+          'owner': 'test',
+        });
+      });
+    });
+  });
+}
