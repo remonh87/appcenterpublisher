77bdade5a193a126d2ad5629bc8f2d67efa5baea
Rémon <remonhelmond@gmail.com>
2019-11-17 15:49:55 +0100

    create commandrunner and upload command

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:app_center_uploader/src/upload_app_command.dart';
+import 'package:args/command_runner.dart';
+
+void main(List<String> args) {
+  CommandRunner<dynamic>('uploader', 'Tooling to upload and distribute app binaries to Appcenter')
+    ..addCommand(UploadAppCommand())
+    ..run(args);
+}
