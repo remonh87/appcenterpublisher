808bccd82d694b72e1409bac5a8466c704cdf03b
Rémon <remonhelmond@gmail.com>
2019-11-17 14:51:26 +0100

    create yaml parser for parsing the congfig

---
+++ lib/src/parse_yaml.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:yaml/yaml.dart' as yaml;
+
+Map<String, String> parseYamlConfig(void Function(Object object) log, File yamlFile) {
+  final fileContents = yamlFile.readAsStringSync();
+  log('Parsing yamlfile: ${yamlFile.path}');
+
+  try {
+    final config = yaml.loadYaml(fileContents) as yaml.YamlMap;
+
+    final convertedMap = config.map((k, v) => MapEntry(k.toString(), v.toString()));
+    return convertedMap;
+  } on Error catch (e) {
+    log('Error parsing yaml failed message: $e');
+    throw ParseConfigException(e.toString());
+  }
+}
+
+class ParseConfigException {
+  const ParseConfigException(this.message);
+
+  final String message;
+}
