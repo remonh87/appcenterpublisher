1dde18ecd560bd41d7325bacb4dad0741c57348f
Rémon <remonhelmond@gmail.com>
2019-11-23 15:36:22 +0100

    Improve readme and example config

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,7 @@ import 'package:appcenterpublisher/src/publish_app_command.dart';
 import 'package:args/command_runner.dart';
 
 void main(List<String> args) {
-  CommandRunner<dynamic>('distributor', 'Tooling to upload and distribute app binaries to Appcenter')
+  CommandRunner<dynamic>('publisher', 'Tooling to upload and distribute apps to Appcenter')
     ..addCommand(PublishAppCommand())
     ..run(args);
 }
